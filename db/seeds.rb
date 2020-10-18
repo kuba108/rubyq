e395209a166eb2bd9c0a1cfdd82a272fc725f7c3
kuba <kuba@elegantniweb.cz>
2020-10-18 20:16:14 +0200

    Adds db seed for admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AdminUser.create! first_name: 'Admin',
+                  last_name: 'Admin',
+                  email: 'admin@example.com',
+                  password: 'admin1234',
+                  acl: {"policies":{"dashboard":{"show_stats":"1"},"admin_user":{"index":"1","show":"1","update":"1","create":"1","destroy":"1","update_acl":"1","change_passwords":"1"},"menu":{"index":"1","show":"1","update":"1","create":"1","destroy":"1","change_order":"1"},"menu_item":{"index":"1","show":"1","update":"1","create":"1","destroy":"1"},"page":{"index":"1","show":"1","update":"1","create":"1","destroy":"1"},"gallery":{"index":"1","show":"1","update":"1","create":"1","destroy":"1"},"gallery_item":{"index":"1","show":"1","update":"1","create":"1","destroy":"1"},"setting":{"index":"1","show":"1","update":"1","create":"1","destroy":"1"}}}
